7116b36a0cb8fd2f1e7c0d134d4fd036ae1786d5
dave-andreas <david.roynaldo.25@gmail.com>
2019-11-07 15:49:31 +0700

    hexakelar

---
+++ hexa.js
@@ -0,0 +1,18 @@
+var hexa=(a)=>{
+    hexaData=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+    output=''
+    for(i=0;i<10000;i++){
+        output+=hexaData[(a%16)]
+        a=Math.floor(a/16)
+        if(a==0){
+            break
+        }
+    }
+    output=(output.split('')).reverse()
+    hasil=''
+    for(i=0;i<output.length;i++){
+        hasil+=output[i]
+    }
+    return hasil
+}
+console.log(hexa(7562))
